0d91196652fc686ce1e22f3041fc9aa83cc04a75
janani76-coder <jananijanani1776@gmail.com>
2024-03-09 17:16:35 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Ftravel-itinerary-design-template_23-2149648602.jpg&tbnid=pdfeEIB9EPrqQM&vet=12ahUKEwil2L2JieeEAxWuyzgGHausDdAQMygAegQIARBM..i&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Fvectors%2Ftravel-planner&docid=YjNLkpMjv-MEYM&w=442&h=626&q=travel%20itinerary%20planner%20images%20free%20open%20source&ved=2ahUKEwil2L2JieeEAxWuyzgGHausDdAQMygAegQIARBM" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend1-ruby.vercel.app"
